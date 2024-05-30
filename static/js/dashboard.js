11cf64dd18c823a58753ae05bbc1ffd6111ee3dd
BuriedInCode <6057651+Buried-In-Code@users.noreply.github.com>
2024-05-30 16:16:14 +1200

    Add support for Feel's like
    Closes #14

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -20,6 +20,7 @@ function createColumn(name) {
   const timeEntry = `<p class="has-text-centered is-pulled-right" id="${name}-time">{time}</p>`;
   const tempEntry = statEntry(`${name}-temp`, "{temp}");
   const humidEntry = statEntry(`${name}-humid`, "{humid}");
+  const feelsEntry = statEntry(`${name}-feels`, "{feels}");
 
   const newColumn = `
     <div class="column is-7">
@@ -29,12 +30,23 @@ function createColumn(name) {
             <h3 class="subtitle is-3 is-pulled-left">${name}</h3>
           </div>
           <div class="column is-half">${timeEntry}</div>
-          <div class="column is-half">${tempEntry}</div>
-          <div class="column is-half">${humidEntry}</div>
+          <div class="column is-one-third">${tempEntry}</div>
+          <div class="column is-one-third">${humidEntry}</div>
+          <div class="column is-one-third">${feelsEntry}</div>
         </div>
       </div>
     </div>`;
   appendToContent(newColumn);
+}
+
+function calculateFeelsLike(temperature, humidity) {
+    temperature = parseFloat(temperature);
+    humidity = parseFloat(humidity) / 100;
+    // Calculate the water vapor pressure
+    const pressure = humidity * 6.105 * Math.exp((17.27 * temperature) / (237.7 + temperature));
+    // Calculate the feels like temperature
+    const feels = temperature + 0.33 * pressure - 4.00;
+    return feels.toFixed(2);
 }
 
 function updateColumn(name, reading) {
@@ -46,6 +58,12 @@ function updateColumn(name, reading) {
 
   const humidLabel = document.getElementById(`${name}-humid`);
   humidLabel.textContent = `${reading.humidity}%`;
+
+  const feelsLabel = document.getElementById(`${name}-feels`);
+  if (reading.temperature === null || reading.humidity === null)
+    feelsLabel.textContent = `${null}°C`;
+  else
+    feelsLabel.textContent = `${calculateFeelsLike(reading.temperature, reading.humidity)}°C`;
 }
 
 async function getCurrentReadings() {
